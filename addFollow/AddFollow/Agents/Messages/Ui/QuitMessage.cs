2bb4e0a5d77179f4684874ccba5ce1ab1bf3f64b
YuvalAlf <u67v67@gmail.com>
2016-07-12 18:01:22 +0300

    Add follow UI

---
+++ addFollow/AddFollow/Agents/Messages/Ui/QuitMessage.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AddFollow.Agents.Messages.Ui
+{
+    public class QuitMessage : UiMessage
+    {
+    }
+}
---
+++ addFollow/AddFollow/Util/Option/NoneOptionExceptioncs.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddFollow.Util.Option
+{
+    public sealed class NoneOptionException : Exception
+    {
+        public NoneOptionException(string message) : base(message)
+        {
+        }
+    }
+}
