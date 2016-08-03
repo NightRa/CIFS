f645bd2c40077beaac7a3f7f02e69e8dbb5ffec2
NightRa <ilan3580@gmail.com>
2016-08-03 15:41:08 +0300

    Clean up tree serialization

--- src/main/scala/serialization/Model.scala
+++ src/main/scala/serialization/Model.scala
@@ -4,8 +4,8 @@ import model._
 import scodec.Codec
 import scodec.codecs._
 
-import scalaz.{IMap, Order}
 import scalaz.std.string._
+import scalaz.{IMap, Order}
 
 object Model {
   // Hash = 256 bits
@@ -21,14 +21,9 @@ object Model {
   def imapCodec[A: Order, B](codecA: Codec[A], codecB: Codec[B]): Codec[IMap[A, B]] = listOfN(int32, codecA ~ codecB).
     xmap[IMap[A, B]](l => IMap(l: _*), _.toList)
 
-  implicit lazy val folderCodec: Codec[Folder] = "Index Folder" |
-    (imapCodec(utf8_32, hashSha256Codec) ~
-      imapCodec(utf8_32, remotePathCodec.as[FollowLeaf]) ~
-      imapCodec(utf8_32, lazily(folderCodec)))
-      .xmap[Folder]({
-      case (((files, follows), folders)) => Folder(files, follows, folders)
-    }, folder => Folder.unapply(folder).get match {
-      case (files, follows, folders) => ((files, follows), folders)
-    })
-  // Todo: understand why .as[Folder] didn't work as before.
+  implicit lazy val folderCodec: Codec[Folder] = "Index Folder" | {
+    imapCodec(utf8_32, hashSha256Codec) ::
+      imapCodec(utf8_32, remotePathCodec.as[FollowLeaf]) ::
+      imapCodec(utf8_32, lazily(folderCodec))
+  }.as[Folder]
 }
